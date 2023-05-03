65dd328c23a8d5fcd500f18565edbfda5a085635
7BrendanJackson <brendan.jackson3@griffithuni.edu.au>
2023-05-04 08:48:32 +1000

    Init

---
+++ Source/GamesStudio/GamesStudioGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GamesStudioGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGamesStudioGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGamesStudioGameMode();
+};
+
+
+
